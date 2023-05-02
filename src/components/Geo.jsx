49f4ffbab89f556e4050c2b5e0f95a4ec1c1f06b
GuoQiang Min <minguoqiang88@outlook.com>
2023-05-02 15:53:25 +0300

    update color of text of geo

--- src/components/Geo.jsx
+++ src/components/Geo.jsx
@@ -4,8 +4,10 @@ import { useFrame } from '@react-three/fiber'
 import { useGLTF, MeshDistortMaterial, Shadow } from '@react-three/drei'
 import Text from './Text'
 import state from '../store/state'
+import { useTheme } from '../hooks/useTheme'
 
 export default function Model(props) {
+  const theme = useTheme()
   const group = useRef()
   const shadow = useRef()
   const { nodes } = useGLTF('/geo.min.glb', true)
@@ -31,9 +33,9 @@ export default function Model(props) {
       <group position={[1.25, -0.5, 0]}>
         <Text position={[0, 0, 0]} fontSize={0.07} lineHeight={1} letterSpacing={-0.05}>
           03
-          <meshBasicMaterial color="#cccccc" toneMapped={false} />
+          <meshBasicMaterial color={theme.palette.text.neutral} toneMapped={false} />
         </Text>
-        <Text bold position={[-0.01, -0.1, 0]} fontSize={0.1} lineHeight={1} letterSpacing={-0.05} color="black">
+        <Text bold position={[-0.01, -0.1, 0]} fontSize={0.1} lineHeight={1} letterSpacing={-0.05} color={theme.palette.text.primary}>
           {`Poimandres,\nThe vision of Hermes`}
         </Text>
       </group>
