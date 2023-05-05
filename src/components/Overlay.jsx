adfceb5576fbd80bd306630e6576e7e9e552f895
GuoQiang Min <minguoqiang88@outlook.com>
2023-05-05 03:01:45 +0300

    Add fog, Implement short menu

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -63,7 +63,23 @@ const LeftMiddle = styled.div`
 //   background: #252525;
 // `
 
-const Hamburger = styled.div`
+// const Hamburger = styled.div`
+//   position: absolute;
+//   display: flex;
+//   flex-direction: column;
+//   top: 1.5vw;
+//   right: 1.5vw;
+//   & > div {
+//     position: relative;
+//     width: 24px;
+//     height: 2px;
+//     background: ${(props) => props.color || '#fff'};
+//     margin-bottom: 6px;
+//   }
+// `
+
+const TopRight = styled.div`
+  font-family: 'Roboto';
   position: absolute;
   display: flex;
   flex-direction: column;
@@ -75,6 +91,69 @@ const Hamburger = styled.div`
     height: 2px;
     background: ${(props) => props.color || '#fff'};
     margin-bottom: 6px;
+  }
+
+  & .nav__list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    //height: 100%;
+    & > .nav__item {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      //height: 100%;
+      &:not(:first-child) {
+        margin-left: 18px;
+      }
+      & > .nav__link {
+        color: #000;
+        text-decoration: none;
+        background-color: transparent;
+        position: relative;
+        display: inline-block;
+        height: 1.7em;
+        font-size: 16px;
+        line-height: 1.7em;
+        &:before {
+          content: '';
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          height: 1px;
+          background-color: #fff;
+          -webkit-transform: scale(0, 1);
+          transform: scale(0, 1);
+          -webkit-transform-origin: right top;
+          transform-origin: right top;
+          -webkit-transition: -webkit-transform 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+          transition: -webkit-transform 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+          transition: transform 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+          transition: transform 0.6s cubic-bezier(0.23, 1, 0.32, 1), -webkit-transform 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+          background-color: #222;
+        }
+        &:hover:before,
+        &:focus:before {
+          -webkit-transform-origin: left top;
+          transform-origin: left top;
+          -webkit-transform: scale(1, 1);
+          transform: scale(1, 1);
+        }
+      }
+    }
   }
 `
 
@@ -88,26 +167,52 @@ export default function Overlay() {
       </TopLeft>
       <BottomLeft>
         <a target="_blank" href="https://www.linkedin.com/in/guoqiang-min" rel="noreferrer">
-          <IconLinkedIn width={36} height={36} color={theme.palette.text.secondary} />
+          <IconLinkedIn width={32} height={32} color={theme.palette.text.secondary} />
         </a>
         <a target="_blank" href="https://github.com/guoqiangmin" rel="noreferrer" style={{ marginLeft: '10px' }}>
-          <IconGithub width={36} height={36} color={theme.palette.text.secondary} />
+          <IconGithub width={32} height={32} color={theme.palette.text.secondary} />
         </a>
       </BottomLeft>
       <BottomRight>
         <a href="mailto:minguoqiang88@gmail.com" rel="noreferrer">
-          <IconMail width={36} height={36} color={theme.palette.text.secondary} />
+          <IconMail width={32} height={32} color={theme.palette.text.secondary} />
         </a>
         <a target="_blank" href="https://wa.me/89684412430" rel="noreferrer" style={{ marginLeft: '10px' }}>
-          <IconWhatsApp width={36} height={36} color={theme.palette.text.secondary} />
+          <IconWhatsApp width={32} height={32} color={theme.palette.text.secondary} />
         </a>
       </BottomRight>
       <LeftMiddle color={theme.palette.text.secondary}>A Personal Portfolio</LeftMiddle>
-      <Hamburger color={theme.palette.text.secondary}>
-        <div />
-        <div />
-        <div />
-      </Hamburger>
+      <TopRight>
+        <nav className="header__nav">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                WORK
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                EXPERTISE
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </TopRight>
+      {/*<Hamburger color={theme.palette.text.secondary}>*/}
+      {/*  <div />*/}
+      {/*  <div />*/}
+      {/*  <div />*/}
+      {/*</Hamburger>*/}
       {/*<Bar />*/}
       {/*<Bar vertical />*/}
     </>
