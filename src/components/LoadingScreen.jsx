6fc511cf3d5eb25614ba37749b9fd6e02dde5ce1
GuoQiang Min <minguoqiang88@outlook.com>
2023-05-30 05:46:05 +0300

    Add loading animation to loading screen overlay

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,28 +1,31 @@
 import { useProgress } from '@react-three/drei'
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export const LoadingScreen = () => {
   const { progress } = useProgress()
+  const [animClass, setAnimClass] = useState('')
+  const timer = useRef(null)
 
   useEffect(() => {
-    let t
     if (progress >= 100) {
       document.body.classList.add('loaded')
-      t = setTimeout(() => {
+
+      timer.current = setTimeout(() => {
         document.body.classList.add('enable-html')
-      }, 2000)
+        setAnimClass('pt-page-moveToBottomEasing')
+      }, 550)
     }
     return () => {
       document.body.classList.remove('loaded')
-      if (t) {
-        clearTimeout(t)
+      if (timer.current) {
+        clearTimeout(timer.current)
         document.body.classList.remove('enable-html')
       }
     }
   }, [progress])
 
   return (
-    <div className={`loading loading-container`}>
+    <div className={`loading loading-container ${animClass}`}>
       <div className={`loading-data`}>{Math.round(progress)}</div>
       <div className="loading-bar-container">
         <div
