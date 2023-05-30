889de9af43cf1c3ae00412af05de6500c892ea59
GuoQiang Min <minguoqiang88@outlook.com>
2023-05-30 03:08:28 +0300

    update portfolio images, title, description and index on slider control panel

--- src/containers/Portfolio.jsx
+++ src/containers/Portfolio.jsx
@@ -16,133 +16,170 @@ const portfolioData = {
   items: [
     {
       image: '/images/portfolio/1.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Personal Portfolio Website',
+      description:
+        'A personal website showcasing past work examples with a responsive 3D layout. Built using React, React Fiber, React Drei, React Spring, Three.js, WebGL, HTML, and CSS.',
     },
     {
       image: '/images/portfolio/2.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Ingenious Vision',
+      description:
+        'A personal website displaying past work examples with a captivating WebGL audio analyzer and floating particles. Built using Three.js, WebGL, HTML, and CSS/SCSS.',
     },
     {
       image: '/images/portfolio/3.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'WebGL Audio Analyzer',
+      description:
+        'A personal work example visualizing an audio analyzer with a mesmerizing blob and noise effect. Built using WebGL and Web Audio API with Three.js.',
     },
     {
       image: '/images/portfolio/4.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Lottery Wheel Game',
+      description:
+        'Built an engaging lottery wheel game with a spinning wheel using Vue.js, Vuex store, Canvas API, and Audio API. Developed with HTML, CSS, and JavaScript.',
     },
     {
       image: '/images/portfolio/5.png',
-      title: 'Shopify Website',
-      description: '',
+      title: '3D Building Presentation',
+      description:
+        'An interactive 3D presentation displaying a building with spot points and animated interactions. Built using JavaScript and Three.js.',
     },
     {
       image: '/images/portfolio/6.png',
-      title: 'Shopify Website',
-      description: '',
+      title: '3D Shirt Configurator',
+      description:
+        'Implemented a realistic 3D shirt configurator with customizable options using Three.js and PBR (Physically Based Rendering). Developed with JavaScript, Three.js, HTML, CSS, and Webpack.',
     },
     {
       image: '/images/portfolio/7.png',
-      title: 'Shopify Website',
-      description: '',
+      title: '3D Horse Nose-band Configurator',
+      description:
+        'Implemented a horse nose-band configurator with measurement visualization for a lifelike representation. Built using JavaScript, Three.js, HTML, CSS, and Webpack.',
     },
     {
       image: '/images/portfolio/8.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Empyrean WebRTC',
+      description:
+        'Implemented a feature-rich screen sharing and live chat system based on WebRTC. Developed with NodeJS/ExpressJS and Socket.io for signaling server and data stream.',
     },
     {
       image: '/images/portfolio/9.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Designhubz XR Widget',
+      description:
+        'Implemented an immersive XR try-on experience for eyewear and makeup using TypeScript, Three.js, and AI integration. Developed with TypeScript, Three.js, Webpack, and WebComponent.',
     },
     {
       image: '/images/portfolio/10.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Designhubz B2B Platform',
+      description:
+        'Enhanced the Designhubz B2B platform with product management and Square online store integration capabilities. Developed with React, TypeScript, Styled Components, Storybook, and AWS services.',
     },
     {
       image: '/images/portfolio/11.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Bionic Trader',
+      description:
+        'Built a cutting-edge bionic trading platform with a visually stunning order book and depth finder using BabylonJS and TypeScript. Developed with NodeJS/ExpressJS, BabylonJS, TypeScript, and various technologies.',
     },
     {
       image: '/images/portfolio/12.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Lightning Bridge',
+      description:
+        'Developed a high-speed altcoin exchange platform running on Lightning Network and Azure infrastructure. Built with React, NextJS, TypeScript, BabylonJS, and PostgreSQL.',
     },
     {
       image: '/images/portfolio/13.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Trade Mango',
+      description:
+        "Integrated the bionic trader's 3D depth finder into the Mango market platform for enhanced trading experience. Developed with React, NextJS, TypeScript, BabylonJS, and Tailwind CSS.",
     },
     {
       image: '/images/portfolio/14.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'SmartCountr Dashboard',
+      description:
+        'Implemented a feature-rich dashboard for SmartCountr, enabling room design and device connectivity visualization. Developed with Vue.js, PIXI.js, Tailwind CSS, and Webpack.',
     },
     {
       image: '/images/portfolio/15.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Sympli Shopify Clothing Store',
+      description:
+        'Integrated a comprehensive size guide, improved collection pages, and optimized site performance for a clothing store on Shopify. Utilized Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/16.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Lunya Clothing Store',
+      description:
+        'Implemented a product kit page, enhanced collection pages, and integrated Klarna payment for a clothing store on Shopify. Developed with Shopify, Shopify Plus, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/17.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Mansur-Gavriel Bag Store',
+      description:
+        'Customized an existing Shopify theme based on a ready-made Figma design, adding color swatches to product and collection pages. Utilized Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/18.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Samuel Hubbard Shoes Store',
+      description:
+        'Resolved mobile compatibility issues and optimized site performance. Implemented color swatches on product and collection pages. Developed with Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/19.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Piglet Bedding Store',
+      description:
+        'Optimized site performance and addressed UI/UX bugs for a bedding, sleepwear, and homeware store. Developed with Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/20.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Bakel Makeup Store',
+      description:
+        'Integrated Recharge and Klarna payment solutions. Fixed UI/UX bugs to enhance the user experience. Developed with Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/21.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Crush Collection Knitwear Store',
+      description:
+        'Built a knitwear store from scratch based on a Figma design. Optimized performance using lazy loading mechanisms. Developed with Shopify, Liquid, HTML, CSS, and JS.',
     },
     {
       image: '/images/portfolio/22.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'World Flight Routes Visualization',
+      description:
+        'Visualized global flight routes on a WebGL-powered map using captivating particle animations. Developed with JavaScript, WebGL, and Three.js.',
     },
     {
       image: '/images/portfolio/23.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Dextr Contact Center',
+      description:
+        'A VOIP application for contact management. Updated the dashboard UI with graph libraries, fixed browser compatibility issues, and resolved bugs. Developed with Vue.js and Amazon Connect.',
     },
     {
       image: '/images/portfolio/24.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'GUESTMAPP Interior Design Service',
+      description:
+        'Revamped page layout using JupiterX Site Builder. Integrated Limmu whiteboard API into the existing app. Developed with Android SDK, Objective C, and Limmu API.',
     },
     {
       image: '/images/portfolio/25.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Refresh Studio',
+      description:
+        'Implemented a WebGL-based website with dynamic effects such as explode particle animations, a moving universe, and drop light effects. Developed with JavaScript, Three.js, and WebGL.',
     },
     {
       image: '/images/portfolio/26.png',
-      title: 'Shopify Website',
-      description: '',
+      title: 'Shoes Configurator',
+      description:
+        'Created a 3D shoes configurator allowing customization of styles, heels, materials, and colors. Developed with JavaScript, Three.js, HTML, and CSS.',
+    },
+    {
+      image: '/images/portfolio/27.png',
+      title: 'Python Video Creator',
+      description: 'Generated hotel promotion videos from a set of images, texts, and brand logos using Python and OpenCV.',
+    },
+    {
+      image: '/images/portfolio/28.png',
+      title: '3D Plans',
+      description:
+        'Developed an interactive 3D working tour for houses using Three.js. Configured the 3D models with planes and wall pictures for an immersive experience. Optimized performance for smooth navigation. Developed with JavaScript, HTML, CSS, and Three.js.',
     },
   ],
 }
@@ -153,6 +190,16 @@ const FormWrapper = styled.div`
   &.show {
     display: block;
   }
+`
+
+const Title = styled.div`
+  font-size: 0.95em;
+  font-weight: 350;
+  //margin-bottom: 1rem;
+  max-width: 15rem;
+  line-height: 1.5rem;
+  user-select: none;
+  color: ${(props) => props.color};
 `
 
 const Description = styled.div`
@@ -217,11 +264,11 @@ const ArrowButton = (props) => {
   )
 }
 
-export function SlideControl({ width, height, onNext, onPrev }) {
+export function SlideControl({ width, height, items, activeIndex, isFirstClick, onNext, onPrev }) {
   const theme = useTheme()
   const { gl, viewport } = useThree()
   const scale = Math.min(1, viewport.width / 16)
-
+  const slideIndex = isFirstClick && isFirstClick.current ? activeIndex : activeIndex + 1
   return (
     <group>
       <mesh>
@@ -237,15 +284,13 @@ export function SlideControl({ width, height, onNext, onPrev }) {
           letterSpacing={0}
           color={theme.palette.text.primary}
           maxWidth={(viewport.width / 4) * 3}>
-          {'01/20'}
+          {`${slideIndex}/${items.length}`}
         </Text>
       </mesh>
       <Html transform portal={{ current: gl.domElement.parentNode }} pointerEvents={'none'}>
         <FormWrapper className={'form-wrapper'}>
-          <Description color={theme.palette.text.primary}>
-            Developed a responsive e-commerce website for a global fashion brand, implementing a sleek UI/UX design, seamless checkout process, and
-            integrated payment gateway for enhanced customer experience and increased conversions. increased conversions. increased conversions.
-          </Description>
+          <Title color={theme.palette.text.primary}>{items[slideIndex - 1].title}:</Title>
+          <Description color={theme.palette.text.primary}>{items[slideIndex - 1].description}</Description>
         </FormWrapper>
       </Html>
     </group>
@@ -337,7 +382,17 @@ export function Portfolio({ onReflow }) {
           )}
         </Box>
         <Box {...{ ...boxProps, maxWidth: 8.2 }}>
-          {(width, height) => <SlideControl width={width} height={height} onNext={onNext} onPrev={onPrev} />}
+          {(width, height) => (
+            <SlideControl
+              width={width}
+              height={height}
+              items={portfolioData.items}
+              activeIndex={currentIndex}
+              isFirstClick={isFirstClick}
+              onNext={onNext}
+              onPrev={onPrev}
+            />
+          )}
         </Box>
       </Box>
     </Box>
